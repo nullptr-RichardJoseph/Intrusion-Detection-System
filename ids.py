94213e2a2bb7a2dace78b771c9c03f4f1558422c
Richie Joseph <Rjoseph5196@conestogac.on.ca>
2024-05-20 15:45:46 -0400

    implimented Basic network capturing

--- ids.py
+++ ids.py
@@ -1,1 +1,13 @@
 # Main IDS application 
+import argparse
+from network.monitoring import start_sniffing
+
+def main():
+    parser=argparse.ArgumentParser(description= "Intrustion Detection System")
+    parser.add_argument('-i', '--interface', type=str, required=True, help ="Network interface to capture packets from")
+    args=parser.parse_args()
+
+    start_sniffing(args.interface)
+
+if __name__=="__main__":
+        main()
--- network/monitoring.py
+++ network/monitoring.py
@@ -1,1 +1,18 @@
 # Module for network monitoring 
+from scapy.all import sniff, IP , TCP
+
+def packet_callback(packet):
+    if IP in packet:
+        ip_src=packet[IP].src
+        ip_dst= packet[IP].dst
+        if TCP in packet:
+            tcp_sport= packet[TCP].sport
+            tcp_dport=packet[TCP].dport
+            print(f" [+] TCP Packet: {ip_src}:{tcp_sport}->{ip_dst}:{tcp_dport}")
+
+        else:
+            print(f"[+] IP Packets: {ip_src}->{ip_dst}")
+
+def start_sniffing(interface):
+    print(f"[*] Starting packet capture on interface {interface}")
+    sniff(iface=interface, prn=packet_callback,store=False)
